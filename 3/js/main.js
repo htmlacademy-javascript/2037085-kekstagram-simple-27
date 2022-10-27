1f39c5283cbe943d526662e2ad0330d75493f1b2
Keks <keks@htmlacademy.ru>
2022-10-27 18:04:25 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,22 +1,16 @@
+/* eslint-disable no-template-curly-in-string */
 //4.15. Больше деталей
-// Массив исходных данных:
-const PHOTO_DESCRIPTION_COUNT = 25;
+const PHOTO_COUNT = 25;
 const MIN_LIKES_COUNT = 15;
 const MAX_LIKES_COUNT = 200;
 const MIN_COMMENTS_COUNT = 0;
 const MAX_COMMENTS_COUNT = 200;
+// Массив исходных данных:
 const DESCRIPTIONS = [
   'Кекс в лесу',
   'Кекс едет к ветеринару',
   'Кекс обедает',
 ];
-const identifiers = [];
-const urls = [];
-
-for (let i = 1; i <= PHOTO_DESCRIPTION_COUNT; i++) {
-  identifiers.push(i);
-  urls.push(`photos/${i}.jpg`);
-}
 
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // Ссылка на источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -30,32 +24,37 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(rand);
 };
 
-getRandomIntInclusive(5, 8);
-
 //Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
-const checkLengthString = (сheckedString, maxLength) => {
-  const rezult = сheckedString.length <= maxLength;
-  return rezult;
+// eslint-disable-next-line no-unused-vars
+const checkLengthString = (checkedString, maxLength) => {
+  const result = checkedString.length <= maxLength;
+  return result;
 };
 
-checkLengthString(' ', 140);
-
 //Функция, возвращающая индекс элемента массива
-function getRandomArrayElement (еlement) {
-  return еlement[getRandomIntInclusive(0, еlement.length - 1)];
-}
-//Функция builder, создающая описание фотографии
-function createPhotoDescription () {
-  return {
-    id: getRandomArrayElement(identifiers),
-    url: getRandomArrayElement(urls),
-    description: getRandomArrayElement(DESCRIPTIONS),
-    likes: getRandomIntInclusive(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
-    comments: getRandomIntInclusive(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT),
-  };
-}
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
 
-//Создание массива заданной длины (с 25 сгенерированными объектами)
-// eslint-disable-next-line no-unused-vars
-const randomPhotoDescription = Array.from({length: 25}, createPhotoDescription);
+//Функция builder, создающая объект 1 фотографии
+const createPhoto = (index) => ({
+  id: index,
+  url: 'photos/${index}.jpg',
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomIntInclusive(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
+  comments: getRandomIntInclusive(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT),
+});
 
+//Создание массива заданной длины - с 25 сгенерированными объектами
+// Функция, создающая много фотографий
+const createPhotos = () => {
+  const result = []; //Результирующий массив
+
+
+  for (let i = 1; i <= PHOTO_COUNT; i++) {
+    const photo = createPhoto(i); // Передаём счётчик
+    result.push(photo);
+  }
+
+  return result; // Возвращаем заполненный массив
+};
+
+createPhotos();
