d094029626e5214571931e40fe1f92b76f7bc1d2
Keks <keks@htmlacademy.ru>
2022-11-02 13:44:36 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,3 @@
+import {renderingPictures} from './picture.js';
+
+renderingPictures();
---
+++ 5/js/util.js
@@ -0,0 +1,25 @@
+//Функция для проверки максимальной длины строки. Будет использоваться для проверки длины введённого комментария
+const checkLengthString = (checkedString, maxLength) => {
+  const result = checkedString.length <= maxLength;
+  return result;
+};
+
+checkLengthString(' ', 140);
+
+//Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// Ссылка на источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0) {
+
+    return NaN;
+  }
+  const rand = min + Math.random() * (max + 1 - min);
+
+  return Math.floor(rand);
+};
+
+//Функция, возвращающая индекс элемента массива
+const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+getRandomArrayElement();
+
+export {getRandomIntInclusive};
