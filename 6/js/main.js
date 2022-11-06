f18a62673bd88300ee72e9bafe248bb4ec3a123c
Keks <keks@htmlacademy.ru>
2022-11-06 21:23:44 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import {renderingPictures} from './picture.js';
+import './form.js';
+
+renderingPictures();
---
+++ 6/js/picture.js
@@ -0,0 +1,24 @@
+import {createPhotos} from './data.js';
+
+const picturesSection = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const pictures = createPhotos();
+
+const renderingPictures = () => {
+
+  const pictureFragment = document.createDocumentFragment();
+
+  pictures.forEach(({url, likes, comments}) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__comments').textContent = comments;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureFragment.appendChild(pictureElement);
+  });
+
+  picturesSection.appendChild(pictureFragment);
+
+};
+
+export {renderingPictures};
